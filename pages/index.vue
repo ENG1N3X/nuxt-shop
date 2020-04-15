500af132eb1b816ca5fdc13ae9d9fea37b47221a
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-15 19:43:33 +0300

    обновлена страница продуктов

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,16 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-shop
-      </h1>
-      <h2 class="subtitle">
-        My brilliant Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  //- choice-start
+  section.container.mb-40
+    .row.mb-10
+      .col-12
+        h2.mainTitle Товары
+    .row.justify-content-center
+      - for(let i = 0; i < 6; i++)
+        .col-4.mb-40
+          <nuxt-link to="/product" class="choice">
+            img(src="~assets/img/products/product1.png", alt="product").img-fluid.choiceImg
+            h4.choiceTitle Мой продукт
+            a.choiceBtn Подробнее
+          </nuxt-link>
+  //- choice-end
 </template>
-
-<script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  display: block;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
