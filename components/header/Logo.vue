82457c9bb4cc9815a4fa9dc4311d8a92f253813d
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-15 19:42:13 +0300

    + папка компонентов header

---
+++ components/header/Logo.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  nuxt-link(to="/").headerLogo
+    img(src="~/assets/img/logo.png", alt="logo").headerLogo__img
+    span.headerLogo__text Наш крутой магазин
+</template>
---
+++ components/header/Nav.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  nav.nav.headerNav
+    n-link.nav-link(to='/') Главная
+    a.nav-link(href='#!') Ещё что-то
+    a.nav-link(href='#!') Ещё что-то2
+</template>
