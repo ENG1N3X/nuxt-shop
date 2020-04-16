3dc4c63dbf5487891d12e149398ca667f110dd0c
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-16 17:43:25 +0300

    две новых страницы

---
+++ pages/admin.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- admin-start -->
+  <section class="container" id="app">
+    <div class="row mb-30">
+      <div class="col-12">
+        <h1 class="mainTitle">Добавить товар</h1>
+      </div>
+    </div>
+    <form class="form-row mb-50">
+      <div class="col-6 d-flex align-items-end">
+        <div class="form-group">
+          <label for="picture">Добавить фото</label>
+          <input type="file" class="form-control-file" id="picture" />
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-row">
+          <div class="form-group col-12">
+            <label for="name">Название товара</label>
+            <input type="text" class="form-control" id="name" />
+          </div>
+          <div class="form-group col-4">
+            <label for="price">Цена товара</label>
+            <input type="number" class="form-control" id="price" />
+          </div>
+          <div class="form-group col-12">
+            <label for="description">Описание товара</label>
+            <textarea class="form-control" id="description" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn submitBtn">Добавить</button>
+    </form>
+    <!-- editProduct-start -->
+    <div class="row align-items-center editProduct" v-for="num in 3" :key="num">
+      <div class="col-6">
+        <h4 class="editProduct__name">
+          Название товара
+        </h4>
+      </div>
+      <div class="col-6">
+        <div class="row">
+          <div class="col-6">
+            <button class="btn editBtn" @click="isActive = !isActive">Редактировать</button>
+          </div>
+          <div class="col-6">
+            <button type="submit" class="btn removeBtn">Удалить</button>
+          </div>
+        </div>
+      </div>
+      <div :class="['col-12 editProduct__block', isActive ? 'active' : '']">
+        <form class="form-row mt-20">
+          <div class="form-group col-6">
+            <label for="price">Изменить название</label>
+            <input type="number" class="form-control" id="price" />
+          </div>
+          <div class="form-group col-6">
+            <label for="price">Изменить цену</label>
+            <input type="number" class="form-control" id="price" />
+          </div>
+          <div class="form-group col-12">
+            <label for="description">Описание товара</label>
+            <textarea class="form-control" id="description" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn submitBtn">Изменить</button>
+        </form>
+      </div>
+    </div>
+    <!-- editProduct-end -->
+  </section>
+  <!-- admin-end -->
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+}
+</script>
---
+++ pages/auth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- auth-start -->
+    <section class="container">
+      <div class="row mb-30">
+        <div class="col-12">
+          <h2 class="mainTitle">Админ-панель</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <form class="login">
+            <!-- required ! -->
+            <div class="form-group">
+              <label for="login">Логин</label>
+              <input type="text" class="form-control" id="login" />
+            </div>
+            <div class="form-group">
+              <label for="password">Пароль</label>
+              <input type="password" class="form-control" id="password" />
+            </div>
+            <!-- <button type="submit" class="btn loginBtn">Вход</button> -->
+            <nuxt-link type="submit" to="/admin" class="btn loginBtn">Вход</nuxt-link>
+          </form>
+        </div>
+      </div>
+    </section>
+    <!-- auth-end -->
+  </div>
+</template>
