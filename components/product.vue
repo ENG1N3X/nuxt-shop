5c7d7718d9bee6c65074006381a624b2aedc126d
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-17 14:21:57 +0300

    Вынесен компонет для продукта и корзины

---
+++ components/product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- product-block-start -->
+    <div class="row align-items-center product">
+      <div class="col-12 mb-50">
+        <h2 class="productTitle">Товар #1</h2>
+      </div>
+      <div class="col-6 text-center">
+        <img class="img-fluid productImg" src="~assets/img/products/product.png" alt="product" />
+      </div>
+      <div class="col-6">
+        <div class="row">
+          <div class="col-6 text-center">
+            <p class="productPrice">500 рублей</p>
+          </div>
+          <div class="col-6 text-center">
+            <p class="productCount">500 штук</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <p class="productDesc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsum, quo itaque esse corporis, incidunt laborum laboriosam porro expedita facere est iste et error nostrum consequuntur ex
+              explicabo tempore sint.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- product-block-end -->
+  </div>
+</template>
