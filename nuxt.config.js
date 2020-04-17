d181afeed089539123d3c4ed6f8c9e1c533521a2
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-17 14:19:54 +0300

    + font-awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  mode: "universal",
+  mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
   /*
    ** Global CSS
    */
-  css: ["~assets/sass/styles.sass"],
+  css: ['~assets/sass/styles.sass', 'font-awesome/css/font-awesome.min.css'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -37,7 +37,7 @@ module.exports = {
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    "bootstrap-vue/nuxt"
+    'bootstrap-vue/nuxt',
   ],
   /*
    ** Build configuration
@@ -46,6 +46,6 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
-};
+    extend(config, ctx) {},
+  },
+}
