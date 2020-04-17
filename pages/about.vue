9d828e7f42be62f8984c53c7616a5e953b0829dc
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-17 14:21:04 +0300

    + новые страницы

---
+++ pages/about.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- about-start -->
+    <section class="container">
+      <div class="row mb-30">
+        <div class="col-12">
+          <h2 class="mainTitle">О нас</h2>
+        </div>
+      </div>
+      <div class="row align-items-center mb-30">
+        <div class="col-6">
+          <img src="~assets/img/about/about1.png" alt="online-shop" class="img-fluid anim-push" />
+        </div>
+        <div class="col-6">
+          <h3>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim voluptas saepe eveniet iusto repellat. Aperiam officiis rem quaerat excepturi, porro laudantium quod suscipit voluptatum consequuntur? Temporibus
+            id dolores distinctio voluptas.
+          </h3>
+        </div>
+      </div>
+      <div class="row align-items-center mb-30">
+        <div class="col-6">
+          <h3>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim voluptas saepe eveniet iusto repellat. Aperiam officiis rem quaerat excepturi, porro laudantium quod suscipit voluptatum consequuntur? Temporibus
+            id dolores distinctio voluptas.
+          </h3>
+        </div>
+        <div class="col-6">
+          <img src="~assets/img/about/about2.png" alt="quick-shop" class="img-fluid anim-push" />
+        </div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-6">
+          <img src="~assets/img/about/about3.png" alt="protected-shop" class="img-fluid anim-push" />
+        </div>
+        <div class="col-6">
+          <h3>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim voluptas saepe eveniet iusto repellat. Aperiam officiis rem quaerat excepturi, porro laudantium quod suscipit voluptatum consequuntur? Temporibus
+            id dolores distinctio voluptas.
+          </h3>
+        </div>
+      </div>
+    </section>
+    <!-- about-end -->
+  </div>
+</template>
---
+++ pages/basket.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- basket-start -->
+    <section class="container">
+      <div class="row mb-30">
+        <div class="col-12">
+          <h2 class="mainTitle">Корзина</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <h4 class="mb-0 color-545">В коризне пусто</h4>
+        </div>
+      </div>
+      <div class="row mb-30">
+        <div class="col-10">
+          <h3 class="d-flex">
+            Итого:
+            <div class="pl-10 color-545">1000 рублей</div>
+          </h3>
+        </div>
+        <div class="col-2">
+          <button class="btn buyBtn">Оплатить</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" v-for="i in 3" :key="i">
+          <app-product></app-product>
+        </div>
+      </div>
+    </section>
+    <!-- basket-end -->
+  </div>
+</template>
+
+<script>
+import AppProduct from '~/components/product.vue'
+export default {
+  components: {
+    AppProduct,
+  },
+}
+</script>
