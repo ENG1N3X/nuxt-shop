3a8c0d1217af0bd220d2bccdfdaa96d940429dd5
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-17 14:22:54 +0300

    слегка обновлены стили

--- pages/product.vue
+++ pages/product.vue
@@ -1,39 +1,30 @@
 <template>
-  <!-- product-start -->
-  <section class="container">
-    <div class="row mb-30">
-      <div class="col-12">
-        <h1 class="mainTitle">Товар #1</h1>
-      </div>
-    </div>
-    <div class="row align-items-center product">
-      <div class="col-6">
-        <img class="img-fluid productImg" src="~assets/img/products/product.png" alt="product" />
-      </div>
-      <div class="col-6">
-        <div class="row">
-          <div class="col-6 text-center">
-            <p class="productPrice">500 рублей</p>
-          </div>
-          <div class="col-6 text-center">
-            <p class="productCount">500 штук</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <p class="productDesc">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsum, quo itaque esse corporis, incidunt laborum laboriosam porro expedita facere est iste et error nostrum consequuntur ex
-              explicabo tempore sint.
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <button class="btn buyBtn">Купить</button>
-          </div>
+  <div>
+    <!-- product-start -->
+    <section class="container">
+      <div class="row mb-30">
+        <div class="col-12">
+          <h1 class="mainTitle">Описание товара</h1>
         </div>
       </div>
-    </div>
-  </section>
-  <!-- product-end -->
+      <div class="row">
+        <div class="col-12">
+          <app-product></app-product>
+        </div>
+        <div class="offset-10 col-2">
+          <button class="btn buyBtn">В корзину</button>
+        </div>
+      </div>
+    </section>
+    <!-- product-end -->
+  </div>
 </template>
+
+<script>
+import AppProduct from '~/components/product.vue'
+export default {
+  components: {
+    AppProduct,
+  },
+}
+</script>
