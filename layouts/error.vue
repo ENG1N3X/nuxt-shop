a34ed39b7b29cbbd7495b7415eeeadf6088e36c1
eng1n3x <eng1n3x.developer@gmail.com>
2020-04-20 13:35:42 +0300

    +Страница ошибок

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404" class="mainTitle">Страница не найдена.</h1>
+    <h1 v-else class="mainTitle">Произошла ошибка.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
